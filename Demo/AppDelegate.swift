af070de3da219e1c288217274ed1adcfad2a2248
Josh N <zetplaner@gmail.com>
2020-02-13 09:46:34 -0600

    Demo project

---
+++ Demo/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  BSChart
+//
+//  Created by ZetPlaner on 3/11/19.
+//  Copyright © 2019 ZetPlaner. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        return true
+    }
+
+}
+
